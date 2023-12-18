31f94bbef0b0e09b2029adcef861b8225b7b0f9a
mykolas555 <mykolas.motuzas@stud.kitm.lt>
2023-12-18 14:51:26 +0200

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,10 @@
+const About = ()=>{
+    return(
+        <div className="container">
+            <h2>Apie Projekta</h2>
+            <p>labai geras react projektas</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,18 @@
+import Post from "../post/Post";
+
+const Posts = (props)=>{
+    return(
+        <div className="container">
+            <div className="row">
+                {props.items.map((post)=>
+                <Post
+                key={post.id}
+                title={post.title}
+                description={post.content}
+                />)}
+            </div>
+        </div>
+    )
+}
+
+export default Posts
